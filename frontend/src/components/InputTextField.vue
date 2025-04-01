3748d84f0c776f27d5c3b16ee7563347bc3abed0
Igor Kozłowski <igor.kozlowski.acc@gmail.com>
2025-04-01 22:08:38 +0200

    Extracted input code into external component. Defined shape of form data.

---
+++ frontend/src/components/InputTextField.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col gap-2">
+      <label :for="name">{{ props.label }}</label>
+      <InputText :id="name" :name="name" type="text" />
+      <Message v-if="invalid" severity="error" size="small" variant="simple">{{ errorMessage }}</Message>
+    </div>
+  </template>
+  
+  <script setup lang="ts">  
+  import InputText from 'primevue/inputtext';
+  const props = defineProps<{
+    label: string
+    name: string
+    errorMessage?: string
+    invalid?: boolean
+  }>();
+  
+  </script>
+  
--- frontend/src/components/ScrapConfigForm.vue
+++ frontend/src/components/ScrapConfigForm.vue
@@ -1,10 +1,11 @@
 <template>
     <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
-    <InputText name="item" type="text" />
-    <InputText name="price" type="text" />
-    <InputText name="priceFraction" type="text" />
-    <InputText name="link" type="text" />
-<Button label="Submit" />
+        <label class="text-xl text-center">Selectors for scraping</label>
+        <InputTextField label="item list" name="itemList" :errorMessage="$form.itemList?.error?.message" :invalid="$form.itemsList?.invalid" />
+        <InputText name="price" type="text" />
+        <InputText name="priceFraction" type="text" />
+        <InputText name="link" type="text" />
+        <Button label="Submit" type="submit" />
     </Form>
 </template>
 
@@ -13,9 +14,10 @@ import InputText from 'primevue/inputtext';
 import { Form } from '@primevue/forms';
 import Button from 'primevue/button';
 import { reactive } from 'vue';
+import InputTextField from './InputTextField.vue'
 
 const initialValues = reactive({
-    item: '',
+    itemList: '',
     price: '',
     priceFraction: '',
     link: ''
@@ -24,8 +26,8 @@ const initialValues = reactive({
 const resolver = ({ values }) => {
     const errors = {};
 
-    if (!values.item) {
-        errors.item = [{ message: 'Item is required.' }];
+    if (!values.itemList) {
+        errors.itemList = [{ message: 'Item is required.' }];
     }
 
     return {
