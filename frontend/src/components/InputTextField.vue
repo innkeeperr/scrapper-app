ffd8d60bdf45fc09d8af6beb7c2cdbee22e5cd9a
Igor Kozłowski <igor.kozlowski.acc@gmail.com>
2025-04-02 22:30:54 +0200

    Made scrap configuration form more user friendly

--- frontend/src/components/InputTextField.vue
+++ frontend/src/components/InputTextField.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="flex flex-col gap-2">
-      <label :for="name">{{ props.label }}</label>
-      <InputText :id="name" :name="name" type="text" />
-      <Message v-if="invalid" severity="error" size="small" variant="simple">{{ errorMessage }}</Message>
-    </div>
+        <FloatLabel class="relative" variant="in">
+            <InputText :id="name" :name="name" type="text" class="w-full" />
+            <label :for="name">{{ props.label }}</label>
+            <Message class="absolute bottom-[-22px] left-[14px]" v-if="invalid" severity="error" size="small" variant="simple">{{ errorMessage }}</Message>
+        </FloatLabel>  
   </template>
   
   <script setup lang="ts">  
   import InputText from 'primevue/inputtext';
+import Message from 'primevue/message';
+import FloatLabel from 'primevue/floatlabel'
+
   const props = defineProps<{
     label: string
     name: string
--- frontend/src/components/ScrapConfigForm.vue
+++ frontend/src/components/ScrapConfigForm.vue
@@ -1,10 +1,10 @@
 <template>
-    <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
+    <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit" class="flex flex-col gap-6 w-full sm:w-56">
         <label class="text-xl text-center">Selectors for scraping</label>
-        <InputTextField label="item list" name="itemList" :errorMessage="$form.itemList?.error?.message" :invalid="$form.itemsList?.invalid" />
-        <InputText name="price" type="text" />
-        <InputText name="priceFraction" type="text" />
-        <InputText name="link" type="text" />
+        <InputTextField label="item list" name="itemList" :errorMessage="$form.itemList?.error?.message" :invalid="$form.itemList?.invalid" />
+        <InputTextField label="price" name="price" :errorMessage="$form.price?.error?.message" :invalid="$form.price?.invalid" />
+        <InputTextField label="price fraction" name="priceFraction" :errorMessage="$form.priceFraction?.error?.message" :invalid="$form.priceFraction?.invalid" />
+        <InputTextField label="link" name="link" :errorMessage="$form.link?.error?.message" :invalid="$form.link?.invalid" />
         <Button label="Submit" type="submit" />
     </Form>
 </template>
@@ -28,6 +28,10 @@ const resolver = ({ values }) => {
 
     if (!values.itemList) {
         errors.itemList = [{ message: 'Item is required.' }];
+    }
+
+    if (!values.price) {
+        errors.price = [{ message: 'Price is required.' }];
     }
 
     return {
