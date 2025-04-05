7e11658459214dd3f3ea859736f239b2bb6b1652
Igor Kozłowski <igor.kozlowski.acc@gmail.com>
2025-04-05 13:00:17 +0200

    Added mongodb and initial connection to database. Connection string is stored in local env

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const scrapeProducts = require("./scraper");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config({ path: ".env.local" });
 
 const app = express();
 const port = 3000;
+
+const uri = process.env.MONGODB_URI;
+
+// MongoDB connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
